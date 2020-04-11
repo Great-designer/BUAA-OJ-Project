0e63350fa87c997ebd88675b70f533d5f72de781
Great-designer <63183399+Great-designer@users.noreply.github.com>
2020-04-11 10:11:34 +0800

    Create 7 Last_Day's dog.c

---
+++ 7 Last_Day's dog.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main()
+{
+	int n;
+	while(scanf("%d",&n)!=EOF)
+	{
+		int i;
+		for(i=0;i<n;i++)
+		{
+			int j;
+			for(j=0;j<2*n-1;j++)
+			{
+				if(j>=i&&j<=2*n-i-2)
+				{
+					printf("#");
+				}
+				else
+				{
+					printf(" ");
+				}
+			}
+			printf("\n");
+		}
+	}
+	return 0;
+}
