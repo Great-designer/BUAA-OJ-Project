0f0d0b918f5acf29c57f1e2caf7bcbf24e7566b1
Great-designer <63183399+Great-designer@users.noreply.github.com>
2020-09-10 09:21:15 +0800

    小小地维护一下

--- 823 Mdd玩积木(中难).c
+++ 823 Mdd玩积木(中难).c
@@ -1,35 +1,44 @@
 #include<stdio.h>
-#define maxn 1000010
-int a[maxn];
-int n, m;
+
+int a[1000010];
+int n,m;
 int k;
-inline int read() {
+
+int read()
+{
 	int k = 0, f = 1;
-	char c = getchar_unlocked();
-	while (c < '0' || c>'9') {
+	char c = getchar();
+	while (c < '0' || c>'9')
+	{
 		if (c == '-')f = -1;
-		c = getchar_unlocked();
+		c = getchar();
 	}
-	while (c >= '0' && c <= '9') {
+	while (c >= '0' && c <= '9')
+	{
 		k = (k << 1) + (k << 3) + c - 48;
-		c = getchar_unlocked();
+		c = getchar();
 	}
 	return k * f;
 }
-inline int Bsearch(int k) {
+
+int Bsearch(int k)
+{
 	if (!n)return -1;
 	if (n == 1)return a[0] == k ? 0 : -1;
 	int lo = 0, hi = n - 1, mi;
-	while (lo <= hi) {
+	while (lo <= hi)
+	{
 		mi = (lo + hi) >> 1;
 		if (k == a[mi])return mi;
-		if (a[0] <= a[mi]) {
+		if (a[0] <= a[mi])
+		{
 			if (a[0] <= k && k < a[mi])
 				hi = mi - 1;
 			else
 				lo = mi + 1;
 		}
-		else {
+		else
+		{
 			if (a[mi] < k && k <= a[n - 1])
 				lo = mi + 1;
 			else
@@ -38,11 +47,15 @@ inline int Bsearch(int k) {
 	}
 	return -1;
 }
-int main() {
+
+int main()
+{
 	n = read(), m = read();
-	for (int i = 0; i < n; ++i)
+	int i;
+	for(i = 0; i < n; ++i)
 		a[i] = read();
-	while (m--) {
+	while (m--)
+	{
 		k = read();
 		puts(Bsearch(k) >= 0 ? "Yes" : "No");
 	}
