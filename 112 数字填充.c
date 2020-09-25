66719d520aa43ff9b3f12d7e4b894758350e1a23
Great-designer <63183399+Great-designer@users.noreply.github.com>
2020-09-25 13:39:29 +0800

    9.25更新至113

---
+++ 112 数字填充.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+short a[35][35];
+int n;
+
+int main()
+{
+    while(~scanf("%d",&n))
+	{
+        int cnt=1;
+        int i;
+        for(i=1;i<=n;++i)
+		{
+			int j;
+            for(j=i;j<=n;++j)
+			{
+                a[i][j]=cnt++;
+            }
+            for(j=i+1;j<=n;++j)
+			{
+                a[j][i]=cnt++;
+            }
+        }
+        for(i=1;i<=n;++i)
+		{
+			int j;
+            for(j=1;j<=n-1;++j)
+			{
+                printf("%d ",a[i][j]);
+            }
+            printf("%d\n",a[i][n]);
+        }
+        putchar('\n');
+    }
+}
---
+++ 113 超市排队.cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+#include<vector>
+
+using namespace std;
+
+int n,a,b;
+int x;
+
+vector<int> g;
+
+int main()
+{
+    while(~scanf("%d%d%d",&n,&a,&b))
+	{
+        g.clear();
+        while(n--)
+		{
+            scanf("%d",&x);
+            g.push_back(x);
+        }
+        g.insert(g.begin()+a-1,-1);
+        g.insert(g.begin()+b-1,-2);
+        int i;
+        for(i=0;i<g.size()-1;++i)
+		{
+            printf("%d ",g[i]);
+        }
+        printf("%d\n",g[g.size()-1]);
+    }
+}
