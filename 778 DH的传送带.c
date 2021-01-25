0ba70262430c83e1b1d0a657cf53f74725e5cffb
Great-designer <63183399+Great-designer@users.noreply.github.com>
2021-01-25 17:31:12 +0800

    更新至778

--- 778 DH的传送带.c
+++ 778 DH的传送带.c
@@ -3,59 +3,78 @@
 
 void write(int x)
 {
-	if (x < 0)putchar('-'), x = -x;
-	if (x > 9)write(x / 10);
-	putchar(x % 10 + 48);
+	if(x<0)
+	{
+		putchar('-');
+		x=-x;
+	}
+	if(x>9)
+	{
+		write(x/10);
+	}
+	putchar(x%10+48);
 }
 
 int read()
 {
-	int k = 0, f = 1;
-	char c = getchar();
-	while (c < '0' || c>'9')
+	int k=0,f=1;
+	char c=getchar();
+	while(c<'0'||c>'9')
 	{
-		if (c == '-')f = -1;
-		c = getchar();
+		if(c=='-')
+		{
+			f=-1;
+		}
+		c=getchar();
 	}
-	while (c >= '0' && c <= '9')
+	while(c>='0'&&c<='9')
 	{
-		k = (k << 1) + (k << 3) + c - 48;
-		c = getchar();
+		k=(k<<1)+(k<<3)+c-48;
+		c=getchar();
 	}
-	return k * f;
+	return k*f;
 }
 
 int a[50000];
-int n, t;
+int n,t;
 
 int main()
 {
-	while (scanf("%d%d", &n, &t) != EOF)
+	while(~scanf("%d%d",&n,&t))
 	{
-		memset(a, 0, sizeof(a));
-		int i; 
-		for(i = 1; i < n; ++i)
+		memset(a,0,sizeof(a));
+		int i;
+		for(i=1;i<n;++i)
 		{
-			a[i] = read();
+			a[i]=read();
 		}
-		int t1 = a[1];
-		int j = 1;
-		int ans = 0;
-		char flag = 0;
-		while (ans < n)
+		int t1=a[1];
+		int j=1;
+		int ans=0;
+		char flag=0;
+		while(ans<n)
 		{
-			ans = t1 + j;
-			if (ans > n)break;
-			j = ans;
-			t1 = a[ans];
-			if (ans == t)
+			ans=t1+j;
+			if(ans>n)
 			{
-				flag = 1;
+				break;
+			}
+			j=ans;
+			t1=a[ans];
+			if(ans==t)
+			{
+				flag=1;
 				break;
 			}
 		}
-		if (flag)puts("YES");
-		else puts("NO");
+		if(flag)
+		{
+			puts("YES");
+		}
+		else
+		{
+			puts("NO");
+		}
 	}
 }
 
