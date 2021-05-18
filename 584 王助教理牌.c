1bb0591a23b0d316fbf19e605ee5b6829bca5a11
Great-designer <63183399+Great-designer@users.noreply.github.com>
2021-05-18 12:03:44 +0800

    加一个题

---
+++ 584 王助教理牌.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<ctype.h>
+
+char s[50];
+char card[50];
+int cnt;
+
+int cmp(const void* p1, const void* p2)
+{
+	char *a = (char *)p1;
+	char *b = (char *)p2;
+	if(*a<*b)
+	{
+		return -1;
+	}
+	else if(*a>*b)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+void print(char *color)
+{
+	memset(card, 0, sizeof(card));
+	cnt = 0;
+	char *start=s,*pos;
+	while((pos = strstr(start, color)) != NULL)
+	{
+		char *ptr = pos;
+		pos--;
+		while(pos - s >= 0 && isdigit(*pos))
+		{
+			card[cnt++] = *pos, --pos;
+		}
+		start = ptr + 1;
+	}
+	if(cnt == 0)
+	{
+		return;
+	}
+	qsort(card, cnt, sizeof(card[0]), cmp);
+	printf("%s%s", card, color);
+}
+
+int main()
+{
+	while(scanf("%s", s) != EOF)
+	{
+		print("m");
+		print("p");
+		print("s");
+		print("z");
+		putchar('\n');
+	}
+}
