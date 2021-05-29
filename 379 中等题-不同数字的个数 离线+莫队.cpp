8617f27bdd5d8262b19b4ca84420b7646e0b69bb
Great-designer <63183399+Great-designer@users.noreply.github.com>
2021-05-29 13:46:29 +0800

    代码简单格式化

--- 379 中等题-不同数字的个数 离线+莫队.cpp
+++ 379 中等题-不同数字的个数 离线+莫队.cpp
@@ -2,66 +2,85 @@
 #include<math.h>
 #include<ctype.h>
 #include<algorithm>
+
 using namespace std;
-typedef long long ll;
-const int maxn = 114514 << 1;
-ll rd() {
-	ll k = 0, f = 1;
+
+long long rd()
+{
+	long long k = 0, f = 1;
 	char c = getchar();
-	while (!isdigit(c)) {
+	while (!isdigit(c))
+	{
 		if (c == '-')f = -1;
 		c = getchar();
 	}
-	while (isdigit(c)) {
+	while (isdigit(c))
+	{
 		k = (k << 1) + (k << 3) + c - 48;
 		c = getchar();
 	}
 	return k * f;
 }
-void wr(ll x) {
+
+void wr(long long x)
+{
 	if (x < 0) putchar('-'), x = -x;
 	if (x > 9)wr(x / 10);
 	putchar(x % 10 + '0');
 }
-ll n, m;
-ll block_sz;
-ll cur, ans[maxn];
-ll cnt[maxn];
-ll a[maxn], disc[maxn], belong[maxn];
 
-struct query {
+long long n, m;
+long long block_sz;
+long long cur, ans[114514 << 1];
+long long cnt[114514 << 1];
+long long a[114514 << 1], disc[114514 << 1], belong[114514 << 1];
+
+struct query
+{
 	int l, r, id;
-	bool operator<(const query& o) const {
+	char operator<(const query& o) const
+	{
 		if (belong[l] == belong[o.l])
-			//return r < o.r; 两个方案任选其一, 后者貌似更快
-			return (belong[l] & 1) ? r < o.r : r > o.r;
+			return (belong[l] & 1) ? r < o.r : r > o.r;//return r < o.r; 两个方案任选其一, 后者貌似更快
 		else return belong[l] < belong[o.l];
 	}
-}q[maxn];
+} q[114514 << 1];
 
-bool cmp(const query& a, const query& b) {
+char cmp(const query& a, const query& b)
+{
 	return a.id < b.id;
 }
 
-void add(int x, int val) {
-	if (val == 1) { ++cnt[x]; if (cnt[x] == 1) ++cur; }
-	else { --cnt[x]; if (cnt[x] == 0) --cur; }
+void add(int x, int val)
+{
+	if (val == 1)
+	{
+		++cnt[x];
+		if (cnt[x] == 1) ++cur;
+	}
+	else
+	{
+		--cnt[x];
+		if (cnt[x] == 0) --cur;
+	}
 }
 
-void solve() {
+void solve()
+{
 	sort(q + 1, q + m + 1);
 	int l = 1, r = 0;
-	for (int i = 1; i <= m; ++i) {
+	for (int i = 1; i <= m; ++i)
+	{
 		while (l < q[i].l) add(a[l], -1), ++l;
 		while (l > q[i].l) --l, add(a[l], 1);
 		while (r < q[i].r) ++r, add(a[r], 1);
 		while (r > q[i].r) add(a[r], -1), --r;
 		ans[q[i].id] = cur;
 	}
-	//sort(q + 1, q + m + 1, cmp);
 }
 
-int main() {
+int main()
+{
 	n = rd(), m = rd();
 	block_sz = sqrt(n);
 	for (int i = 1; i <= n; i++)disc[i] = a[i] = rd();
--- 445 模式寻对 莫队.cpp
+++ 445 模式寻对 莫队.cpp
@@ -1,57 +1,71 @@
-#include<cstdio>
-#include<cstring>
-#include<cstdlib>
-#include<cctype>
-#include<cmath>
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<ctype.h>
+#include<math.h>
 #include<algorithm>
-typedef long long ll;
-const int maxn = 10005;
+
 using namespace std;
-ll rd() {
-	ll k = 0, f = 1;
+
+long long rd()
+{
+	long long k = 0, f = 1;
 	char c = getchar();
-	while (!isdigit(c)) {
+	while (!isdigit(c))
+	{
 		if (c == '-')f = -1;
 		c = getchar();
 	}
-	while (isdigit(c)) {
+	while (isdigit(c))
+	{
 		k = (k << 1) + (k << 3) + c - 48;
 		c = getchar();
 	}
 	return k * f;
 }
-void wr(ll x) {
+
+void wr(long long x)
+{
 	if (x < 0) putchar('-'), x = -x;
 	if (x > 9)wr(x / 10);
 	putchar(x % 10 + '0');
 }
 
-ll now, ans[maxn];
+long long now, ans[10005];
 int n, m;
-ll a[maxn], disc[maxn], belong[maxn];
-ll t[maxn];//BIT
-struct query { 
-	int l, r, id; 
-	bool operator<(const query& o) const {
+long long a[10005], disc[10005], belong[10005];
+long long t[10005];//BIT
+
+struct query
+{
+	int l, r, id;
+	char operator<(const query& o) const
+	{
 		if (belong[l] == belong[o.l])return r < o.r;
 		return belong[l] < belong[o.l];
 	}
-}q[50005];
+} q[50005];
 
-void add(int x, int val) {
+void add(int x, int val)
+{
 	for (int i = x; i <= n; i += i & -i)
 		t[i] += val;
 }
-ll query(int x) {
-	ll sum = 0;
+
+long long query(int x)
+{
+	long long sum = 0;
 	for (int i = x; i; i -= i & -i)
 		sum += t[i];
 	return sum;
 }
-void solve() {
+
+void solve()
+{
 	sort(q + 1, q + m + 1);
 	int l = 1, r = 0;
-	for (int i = 1; i <= m; i++) {
+	for (int i = 1; i <= m; i++)
+	{
 		while (l < q[i].l)
 			add(a[l], -1), now -= query(a[l] - 1), l++;
 		while (r > q[i].r)
@@ -63,7 +77,9 @@ void solve() {
 		ans[q[i].id] = now;
 	}
 }
-void init() {
+
+void init()
+{
 	memset(ans, 0, sizeof(ans));
 	memset(a, 0, sizeof(a));
 	memset(disc, 0, sizeof(disc));
@@ -72,10 +88,13 @@ void init() {
 	memset(q, 0, sizeof(q));
 	now = 0;
 }
-int main() {
-	while (scanf("%d", &n) != EOF) {
+
+int main()
+{
+	while (scanf("%d", &n) != EOF)
+	{
 		init();
-		ll t = sqrt(n);
+		long long t = sqrt(n);
 		for (int i = 1; i <= n; i++)disc[i] = a[i] = rd();
 		sort(disc + 1, disc + n + 1);
 		int real_n = unique(disc + 1, disc + n + 1) - disc - 1;
