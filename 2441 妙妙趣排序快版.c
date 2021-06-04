4b963cf4012a6fe668c52eae25772c126cde1b35
Great-designer <63183399+Great-designer@users.noreply.github.com>
2021-06-04 14:56:45 +0800

    调整代码格式

--- 2441 妙妙趣排序快版.c
+++ 2441 妙妙趣排序快版.c
@@ -1,7 +1,6 @@
 #include<stdio.h>
 
 int map[55];
-int ori[55];
 
 int n,k,tmp,ans;
 
@@ -12,85 +11,35 @@ struct node
 
 struct node node[11];
 
+void swap(int *a,int *b)
+{
+	int temp=*a;
+	*a=*b;
+	*b=temp;
+}
+
 void dfs(int step)
 {
-	if(step==1)
-	{
-		int i;
-		for(i=1; i<=n; i++)
-		{
-			ori[i]=map[i]; /* 其实也可以不储存，因为有回溯，不会打乱 */
-		}
-	}
-	if(step==k+1)
+	if(step==k+1)//经过了所有筛选器筛选，所以++
 	{
 		ans++;
 		return;
 	}
 	int num=k-step+1;
-	if(ori[node[num].l]<ori[node[num].r])
+	if(map[node[num].l]<map[node[num].r])//可能是经过这个筛选器得到的数列
 	{
+		dfs(step+1);//判断前一个筛选器
+		swap(&map[node[num].l],&map[node[num].r]);//交换后再进行判断
 		dfs(step+1);
-		tmp=ori[node[num].l];
-		ori[node[num].l]=ori[node[num].r];
-		ori[node[num].r]=tmp;
-		dfs(step+1);
-		tmp=ori[node[num].l];
-		ori[node[num].l]=ori[node[num].r];
-		ori[node[num].r]=tmp;
-	}
-	return;
-}
-
-void func1()
-{
-	int i;
-	for(i=1; i<=n-1; i++) /* 从i进行交换 */
-	{
-		int j;
-		for(j=1; j<=n; j++)
-		{
-			map[j]=j;
-		}
-		for(j=1; j<=n-i; j++)
-		{
-			tmp=map[i+j-1];
-			map[i+j-1]=map[i+j];
-			map[i+j]=tmp;
-			dfs(1);
-		}
-	}
-}
-
-void func2()
-{
-	int i;
-	for(i=2; i<=n-1; i++)
-	{
-		int j;
-		for(j=1; j<=n; j++)
-		{
-			map[j]=j;
-		}
-		tmp=map[i+1];
-		map[i+1]=map[i];
-		map[i]=tmp;
-		for(j=1; j<=i-1; j++)
-		{
-			tmp=map[i-j+1];
-			map[i-j+1]=map[i-j];
-			map[i-j]=tmp;
-			dfs(1);
-		}
+		swap(&map[node[num].l],&map[node[num].r]);//交换回来防止出错
 	}
 }
 
 int main()
 {
-	int t;
-	scanf("%d",&t);
-	int i;
-	for(i=1; i<=t; i++)
+	int cnt;
+	scanf("%d",&cnt);
+	while(cnt--)
 	{
 		ans=0;
 		scanf("%d%d",&n,&k);
@@ -99,8 +48,32 @@ int main()
 		{
 			scanf("%d%d",&node[j].l,&node[j].r);
 		}
-		func1();
-		func2();
+		int i;
+		for(i=1; i<=n-1; i++)//从i进行交换
+		{
+			for(j=1; j<=n; j++)
+			{
+				map[j]=j;
+			}
+			for(j=1; j<=n-i; j++)
+			{
+				swap(&map[i+j-1],&map[i+j]);
+				dfs(1);
+			}
+		}
+		for(i=2; i<=n-1; i++)
+		{
+			for(j=1; j<=n; j++)
+			{
+				map[j]=j;
+			}
+			swap(&map[i+1],&map[i]);
+			for(j=1; j<=i-1; j++)
+			{
+				swap(&map[i-j+1],&map[i-j]);
+				dfs(1);
+			}
+		}
 		for(j=1; j<=n; j++)
 		{
 			map[j]=j;
--- 2441 妙妙趣排序慢版.c
+++ 2441 妙妙趣排序慢版.c
@@ -3,9 +3,15 @@
 int filter[11][2];
 long long ans = 0;
 
+void swap(int *a,int *b)
+{
+	int temp=*a;
+	*a=*b;
+	*b=temp;
+}
+
 void dfs(int t,int a[])
 {
-	int temp;
 	if (t==0)  //经过了所有筛选器筛选，所以++
 	{
 		ans++;
@@ -13,23 +19,12 @@ void dfs(int t,int a[])
 	}
 	int l = filter[t][0];
 	int r = filter[t][1];
-	if (a[l] > a[r])//不可能是经过这个筛选器得到的数列，所以直接返回
+	if (a[l]<=a[r])//可能是经过这个筛选器得到的数列
 	{
-		return ;
-	}
-	else
-	{
-		//判断前一个筛选器
+		dfs(t-1,a);//判断前一个筛选器
+		swap(&a[l],&a[r]);//交换后再进行判断
 		dfs(t-1,a);
-		//交换后再进行判断
-		temp = a[l];
-		a[l] = a[r];
-		a[r] = temp;
-		dfs(t-1,a);
-		//交换回来防止出错
-		temp = a[r];
-		a[r] = a[l];
-		a[l] = temp;
+		swap(&a[l],&a[r]);//交换回来防止出错
 	}
 }
 
@@ -50,8 +45,7 @@ int main()
 		{
 			scanf("%d%d",&filter[i][0],&filter[i][1]);
 		}
-		//准备构建即将排好序的数列，把1-n分别放到对角线
-		for (i=1; i<=n; i++)
+		for (i=1; i<=n; i++)//准备构建即将排好序的数列，把1-n分别放到对角线
 		{
 			int j;
 			for (j=1; j<=n; j++)
@@ -59,8 +53,7 @@ int main()
 				num[(i-1)*n+j][j] = i;//(i-1)*n+j就是自动随着j向下移动
 			}
 		}
-		//补充空白的位置，按顺序把剩下的填进去
-		for (i=1; i<=n; i++)
+		for (i=1; i<=n; i++)//补充空白的位置，按顺序把剩下的填进去
 		{
 			int j;
 			for (j=1; j<=n; j++)
@@ -68,34 +61,38 @@ int main()
 				for (int k=1,temp=1; k<=n; k++)
 				{
 					if (temp==i)
+					{
 						temp++;//是填入的值所以跳过
-
-					if (k==j)
-						continue;//已经有之前填入的对角线了
-
-					num[(i-1)*n+j][k] = temp++;
+					}
+					if (k!=j)
+					{
+						num[(i-1)*n+j][k] = temp++;//没有之前填入的对角线
+					}
 				}
 			}
 		}
-		//将重复的数列进行屏蔽处理
-		for (i=1; i<=N; i++)
+		for (i=1; i<=N; i++)//将重复的数列进行屏蔽处理
 		{
 			int j;
-			for (j=i+1; j<=N; j++)
+			for (j=i+1; j<=N; j++)//遍历判断是否相同
 			{
-				//遍历判断是否相同
 				int k = 1;
-				for(; num[j][k]==num[i][k]&&k<=n; k++) {}
+				while(num[j][k]==num[i][k]&&k<=n)
+				{
+					k++;
+				}
 				if(k==n+1)
+				{
 					num[j][1] = -1;
+				}
 			}
 		}
-		//分别对即将排好序的数列进行反向筛选
-		for (i=1; i<=N; i++)
+		for (i=1; i<=N; i++)//分别对即将排好序的数列进行反向筛选
 		{
-			if (num[i][1] == -1)
-				continue;
-			dfs(fcnt,num[i]);
+			if (num[i][1]!=-1)
+			{
+				dfs(fcnt,num[i]);
+			}
 		}
 		printf("%d\n",ans);
 	}
