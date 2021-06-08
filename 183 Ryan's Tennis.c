d568f04b61a48028f45a08f51ab0e6d8bd9b0240
Great-designer <63183399+Great-designer@users.noreply.github.com>
2021-06-08 18:45:50 +0800

    更新题目

---
+++ 183 Ryan's Tennis.c
@@ -0,0 +1,363 @@
+#include<stdio.h>
+#include<string.h>
+
+char s[114514];
+int scores[] = { 0, 15, 30, 40 };
+
+int main()
+{
+	while (scanf("%s",s) !=EOF)
+	{
+		int A_Set_Score = 0;
+		int B_Set_Score = 0;
+		int A_Game_Score = 0;
+		int B_Game_Score = 0;
+		int A_Ball = 0;
+		int B_Ball = 0;
+		int Set_Type = 1;
+		int Game_Type = 1;
+		for (int i = 0; i < strlen(s); i++)
+		{
+			if (Game_Type == 1)
+			{
+				if (s[i] == 'A')
+				{
+					if (A_Ball == -1)
+					{
+						A_Game_Score++;
+						if (A_Game_Score == 6)
+						{
+							A_Set_Score++;
+							A_Game_Score = 0;
+							B_Game_Score = 0;
+						}
+						A_Ball = 0;
+						B_Ball = 0;
+					}
+					else if (B_Ball == -1)
+					{
+						A_Ball = 3;
+						B_Ball = 3;
+					}
+					else
+					{
+						A_Ball++;
+						if (A_Ball >= 4)
+						{
+							if (B_Ball < 3)
+							{
+								A_Game_Score++;
+								if (A_Game_Score == 6)
+								{
+									A_Set_Score++;
+									A_Game_Score = 0;
+									B_Game_Score = 0;
+								}
+								A_Ball = 0;
+								B_Ball = 0;
+							}
+							else
+							{
+								A_Ball = -1;
+							}
+						}
+					}
+				}
+				else
+				{
+					if (B_Ball == -1)
+					{
+						B_Game_Score++;
+						if (B_Game_Score == 6)
+						{
+							B_Set_Score++;
+							A_Game_Score = 0;
+							B_Game_Score = 0;
+						}
+						A_Ball = 0;
+						B_Ball = 0;
+					}
+					else if (A_Ball == -1)
+					{
+						A_Ball = 3;
+						B_Ball = 3;
+					}
+					else
+					{
+						B_Ball++;
+						if (B_Ball >= 4)
+						{
+							if (A_Ball < 3)
+							{
+								B_Game_Score++;
+								if (B_Game_Score == 6)
+								{
+									B_Set_Score++;
+									A_Game_Score = 0;
+									B_Game_Score = 0;
+								}
+								A_Ball = 0;
+								B_Ball = 0;
+							}
+							else
+							{
+								B_Ball = -1;
+							}
+						}
+					}
+				}
+				printf("  Set:%3d :%3d\n", A_Set_Score, B_Set_Score);
+				printf(" Game:%3d :%3d\n", A_Game_Score, B_Game_Score);
+				if(A_Ball == -1)
+					printf("Point: AD :   \n");
+				else if(B_Ball == -1)
+					printf("Point:    : AD\n");
+				else
+					printf("Point:%3d :%3d\n", scores[A_Ball], scores[B_Ball]);
+			}
+			else if (Game_Type == 2)
+			{
+				if (s[i] == 'A')
+				{
+					if (A_Ball == -1)
+					{
+						A_Game_Score++;
+						if (A_Game_Score == 7)
+						{
+							A_Set_Score++;
+							A_Game_Score = 0;
+							B_Game_Score = 0;
+							Game_Type = 1;
+						}
+						A_Ball = 0;
+						B_Ball = 0;
+					}
+					else if (B_Ball == -1)
+					{
+						A_Ball = 3;
+						B_Ball = 3;
+					}
+					else
+					{
+						A_Ball++;
+						if (A_Ball >= 4)
+						{
+							if (B_Ball < 3)
+							{
+								A_Game_Score++;
+								if (A_Game_Score == 7)
+								{
+									A_Set_Score++;
+									A_Game_Score = 0;
+									B_Game_Score = 0;
+									Game_Type = 1;
+								}
+								A_Ball = 0;
+								B_Ball = 0;
+							}
+							else
+							{
+								A_Ball = -1;
+							}
+						}
+					}
+				}
+				else
+				{
+					if (B_Ball == -1)
+					{
+						B_Game_Score++;
+						if (B_Game_Score == 7)
+						{
+							B_Set_Score++;
+							A_Game_Score = 0;
+							B_Game_Score = 0;
+							Game_Type = 1;
+						}
+						A_Ball = 0;
+						B_Ball = 0;
+					}
+					else if (A_Ball == -1)
+					{
+						A_Ball = 3;
+						B_Ball = 3;
+					}
+					else
+					{
+						B_Ball++;
+						if (B_Ball >= 4)
+						{
+							if (A_Ball < 3)
+							{
+								B_Game_Score++;
+								if (B_Game_Score == 7)
+								{
+									B_Set_Score++;
+									A_Game_Score = 0;
+									B_Game_Score = 0;
+									Game_Type = 1;
+								}
+								A_Ball = 0;
+								B_Ball = 0;
+							}
+							else
+							{
+								B_Ball = -1;
+							}
+						}
+					}
+				}
+				printf("  Set:%3d :%3d\n", A_Set_Score, B_Set_Score);
+				printf(" Game:%3d :%3d\n", A_Game_Score, B_Game_Score);
+				if (A_Ball == -1)
+					printf("Point: AD :   \n");
+				else if (B_Ball == -1)
+					printf("Point:    : AD\n");
+				else
+					printf("Point:%3d :%3d\n", scores[A_Ball], scores[B_Ball]);
+			}
+			else  //3
+			{
+				if (Set_Type == 1)
+				{
+					if (s[i] == 'A')
+					{
+						A_Ball++;
+						if (A_Ball >= 7 && A_Ball - B_Ball >= 2)
+						{
+							A_Set_Score++;
+							A_Game_Score = 0;
+							B_Game_Score = 0;
+							A_Ball = 0;
+							B_Ball = 0;
+							Game_Type = 1;
+						}
+					}
+					else
+					{
+						B_Ball++;
+						if (B_Ball >= 7 && B_Ball - A_Ball >= 2)
+						{
+							B_Set_Score++;
+							A_Game_Score = 0;
+							B_Game_Score = 0;
+							A_Ball = 0;
+							B_Ball = 0;
+							Game_Type = 1;
+						}
+					}
+					printf("  Set:%3d :%3d\n", A_Set_Score, B_Set_Score);
+					printf(" Game:%3d :%3d\n", A_Game_Score, B_Game_Score);
+					printf("Point:%3d :%3d\n", A_Ball, B_Ball);
+				}
+				else
+				{
+					if (s[i] == 'A')
+					{
+						if (A_Ball == -1)
+						{
+							A_Game_Score++;
+							if (A_Game_Score >= 8 && A_Game_Score - B_Game_Score >= 2)
+							{
+								A_Set_Score++;
+								A_Game_Score = 0;
+								B_Game_Score = 0;
+							}
+							A_Ball = 0;
+							B_Ball = 0;
+						}
+						else if (B_Ball == -1)
+						{
+							A_Ball = 3;
+							B_Ball = 3;
+						}
+						else
+						{
+							A_Ball++;
+							if (A_Ball >= 4)
+							{
+								if (B_Ball < 3)
+								{
+									A_Game_Score++;
+									if (A_Game_Score >= 8 && A_Game_Score - B_Game_Score >= 2)
+									{
+										A_Set_Score++;
+										A_Game_Score = 0;
+										B_Game_Score = 0;
+									}
+									A_Ball = 0;
+									B_Ball = 0;
+								}
+								else
+								{
+									A_Ball = -1;
+								}
+							}
+						}
+					}
+					else
+					{
+						if (B_Ball == -1)
+						{
+							B_Game_Score++;
+							if (B_Game_Score >= 8 && B_Game_Score - A_Game_Score >= 2)
+							{
+								B_Set_Score++;
+								A_Game_Score = 0;
+								B_Game_Score = 0;
+							}
+							A_Ball = 0;
+							B_Ball = 0;
+						}
+						else if (A_Ball == -1)
+						{
+							A_Ball = 3;
+							B_Ball = 3;
+						}
+						else
+						{
+							B_Ball++;
+							if (B_Ball >= 4)
+							{
+								if (A_Ball < 3)
+								{
+									B_Game_Score++;
+									if (B_Game_Score >= 8 && B_Game_Score - A_Game_Score >= 2)
+									{
+										B_Set_Score++;
+										A_Game_Score = 0;
+										B_Game_Score = 0;
+									}
+									A_Ball = 0;
+									B_Ball = 0;
+								}
+								else
+								{
+									B_Ball = -1;
+								}
+							}
+						}
+					}
+					printf("  Set:%3d :%3d\n", A_Set_Score, B_Set_Score);
+					printf(" Game:%3d :%3d\n", A_Game_Score, B_Game_Score);
+					if (A_Ball == -1)
+						printf("Point: AD :   \n");
+					else if (B_Ball == -1)
+						printf("Point:    : AD\n");
+					else
+						printf("Point:%3d :%3d\n", scores[A_Ball], scores[B_Ball]);
+				}
+			}
+			if (A_Game_Score == 5 && B_Game_Score == 5)
+				Game_Type = 2;
+			if (A_Game_Score == 6 && B_Game_Score == 6)
+				Game_Type = 3;
+			if (A_Set_Score == 2 && B_Set_Score == 2)
+				Set_Type = 2;
+			if (A_Set_Score == 3)
+				printf("A wins!");
+			if (B_Set_Score == 3)
+				printf("B wins!");
+		}
+	}
+}
---
+++ 187 就决定是你了！.c
@@ -0,0 +1,128 @@
+#include<stdio.h>
+#include<string.h>
+
+int n, m, tmpw = 0, tmpn;
+int w[505], v[505], b[505], c[505], f[505][5 * 505], d[505];
+char mapp[505][505];
+
+void write(int x)
+{
+	if (x < 0)
+	{
+		putchar('-');
+		x = -x;
+	}
+	if (x > 9)write(x / 10);
+	putchar(x % 10 + 48);
+}
+
+int read()
+{
+	int k = 0, f = 1;
+	char c = getchar();
+	while (c < '0' || c>'9')
+	{
+		if (c == '-')f = -1;
+		c = getchar();
+	}
+	while (c >= '0' && c <= '9')
+	{
+		k = (k << 1) + (k << 3) + c - 48;
+		c = getchar();
+	}
+	return k * f;
+}
+
+void init()
+{
+	tmpw = 0;
+	memset(mapp, 0, sizeof(mapp));
+	memset(w, 0, sizeof(w));
+	memset(v, 0, sizeof(v));
+	memset(c, 0, sizeof(c));
+	memset(b, 0, sizeof(b));
+	memset(d, 0, sizeof(d));
+	memset(f, 0, sizeof(f));
+}
+//图论判环
+void floyd()
+{
+	for (int i = 1; i <= n; i++) //弗洛里德判断是否有环；
+		for (int j = 1; j <= n; j++)
+			for (int k = 1; k <= n; k++)
+				if (mapp[k][i] == 1 && mapp[i][j] == 1)
+					mapp[k][j] = 1;
+}
+
+void merge()  //合点
+{
+	tmpn = n;
+	for (int i = 1; i <= tmpn; i++)
+		for (int j = 1; j <= tmpn; j++)
+		{
+			if (mapp[i][j] == 1 && mapp[j][i] == 1 && i != j && w[i] > 0 && w[j] > 0)//如果是新环；
+			{
+				tmpn++;
+				v[tmpn] = v[i] + v[j];
+				w[tmpn] = w[i] + w[j];
+				tmpw--;
+				w[i] = tmpw;
+				w[j] = tmpw;    //tmpw+tmpn永远等于最开始的n
+			}
+
+			//如果j依赖的点被合并(是旧环),且j在环里
+			if (w[d[j]] < 0 && w[j]>0 && mapp[j][d[j]] == 1 && mapp[j][d[j]] == 1)
+			{
+				w[n - w[d[j]]] += w[j];
+				v[n - w[d[j]]] += v[j];
+				w[j] = w[d[j]];
+			}
+
+			//如果j依赖的点在环里，但是j不在环里
+			if (w[d[j]] < 0 && w[j]>0)
+				if ((mapp[j][d[j]] == 1 && mapp[d[j]][j] == 0) || (mapp[j][d[j]] == 0 && mapp[d[j]][j] == 1))
+					d[j] = n - w[d[j]];
+		}
+}
+//树形DP
+int  dfs(int x, int k)
+{
+	if (f[x][k] > 0)    return(f[x][k]);
+	if (x == 0 || k <= 0)    return(0);
+	//不取x
+	f[b[x]][k] = dfs(b[x], k);
+	f[x][k] = f[b[x]][k];
+	int y = k - w[x];
+	for (int i = 0; i <= y; i++)
+	{
+		f[c[x]][y - i] = dfs(c[x], y - i);
+		f[b[x]][i] = dfs(b[x], i);
+		f[x][k]=f[x][k]>(v[x] + f[c[x]][y - i] + f[b[x]][i])?f[x][k]:(v[x] + f[c[x]][y - i] + f[b[x]][i]);
+	}
+	return(f[x][k]);
+}
+
+int main()
+{
+	while (scanf("%d%d",&n,&m) != EOF)
+	{
+		init();
+		for (int i = 1; i <= n; i++)
+			w[i] = read();
+		for (int i = 1; i <= n; i++)
+			v[i] = read();
+		for (int i = 1; i <= n; i++)
+		{
+			d[i] = read();
+			mapp[d[i]][i] = 1;
+		}
+		floyd();
+		merge();
+		//多叉转二叉
+		for (int i = 1; i <= tmpn; i++)
+			if (w[i] > 0)
+				b[i] = c[d[i]], c[d[i]] = i;
+
+		write(dfs(c[0], m)), putchar('\n');
+	}
+}
