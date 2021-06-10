4fdfc5be373a04f9c4f20726c125284fb600c3ad
Great-designer <63183399+Great-designer@users.noreply.github.com>
2021-06-10 14:19:55 +0800

    更改文件类型

--- 4120 （中等题）W形串.c
+++ 4120 （中等题）W形串.c
@@ -1,35 +1,41 @@
 #include<stdio.h>
-#define max(a,b) a>b?a:b
+
 int a[105];
+
 int main()
 {
 	int t;
 	scanf("%d",&t);
-	while(t--){
+	while(t--)
+	{
 		int n,ans=1;
 		int len=1;
 		scanf("%d",&n);
-		for(int i=0;i<n;i++){
+		int i;
+		for(i=0; i<n; i++)
+		{
 			scanf("%d",&a[i]);
 		}
 		int flag=0;
-		for(int i=0;i<n-1;i++){
-			
-			if(flag&&(a[i-1]>a[i])^(a[i]>a[i+1])){
+		for(i=0; i<n-1; i++)
+		{
+			if(flag&&(a[i-1]>a[i])^(a[i]>a[i+1]))
+			{
 				len++;
 			}
-			else{
-				ans=max(ans,len);
+			else
+			{
+				ans=(ans>len)?ans:len;
 				len=1;
 				flag=0;
 			}
-			if(a[i]>a[i+1]&&flag==0){
+			if(a[i]>a[i+1]&&flag==0)
+			{
 				flag=1;
 				len++;
 			}
-			
 		}
-		ans=max(ans,len);
+		ans=(ans>len)?ans:len;
 		printf("%d\n",ans);
 	}
 	return 0;
