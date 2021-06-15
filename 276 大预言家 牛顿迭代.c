409a3ecd287b31d1750a3ca5948123e08faf50c2
Great-designer <63183399+Great-designer@users.noreply.github.com>
2021-06-15 20:25:56 +0800

    Update 276 大预言家 牛顿迭代.c

--- 276 大预言家 牛顿迭代.c
+++ 276 大预言家 牛顿迭代.c
@@ -1,15 +1,28 @@
 #include<stdio.h>
-int int_sqrt(int x) {
-	if (x <= 1) return x;
-	int ret = x, rett = (ret + (x / ret)) >> 1;
-	while (rett < ret) {
-		ret = rett;
-		rett = (ret + (x / ret)) >> 1;
+
+int int_sqrt(int x)
+{
+	if(x<=1)
+	{
+		return x;
+	}
+	int ret=x;
+	int rett=(ret+(x/ret))>>1;
+	while(rett<ret)
+	{
+		ret=rett;
+		rett=(ret+(x/ret))>>1;
 	}
 	return ret;
 }
+
 int n;
-int main() {    
-    while(scanf("%d", &n)!=EOF)printf("%d\n",int_sqrt(n));
-    return 0;
+
+int main()
+{
+	while(scanf("%d",&n)!=EOF)
+	{
+		printf("%d\n",int_sqrt(n));
+	}
+	return 0;
 }
