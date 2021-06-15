d0eea68957bac5e29e7eb7db92aaa7506891f83d
Great-designer <63183399+Great-designer@users.noreply.github.com>
2021-06-15 18:15:23 +0800

    整理格式

--- 566 密码安全 单调栈.c
+++ 566 密码安全 单调栈.c
@@ -1,83 +1,90 @@
-#include <ctype.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
-#define maxn 114514
-#define BIT 30
-typedef long long ll;
-const ll mod = 1000000061ll;
-int rd() {
-    int k = 0, f = 1;
-    char c = getchar();
+#include<ctype.h>
+#include<stdio.h>
+#include<string.h>
 
-    while (!isdigit(c)) {
-        if (c == '-')
-            f = -1;
-
-        c = getchar();
-    }
-
-    while (isdigit(c)) {
-        k = (k << 1) + (k << 3) + c - 48;
-        c = getchar();
-    }
-
-    return k * f;
+int rd()
+{
+	int k = 0, f = 1;
+	char c = getchar();
+	while (!isdigit(c))
+	{
+		if (c == '-')
+		{
+			f = -1;
+		}
+		c = getchar();
+	}
+	while (isdigit(c))
+	{
+		k = (k << 1) + (k << 3) + c - 48;
+		c = getchar();
+	}
+	return k * f;
 }
-void wr(int x) {
-    if (x < 0)
-        putchar('-'), x = -x;
 
-    if (x > 9)
-        wr(x / 10);
-
-    putchar(x % 10 + '0');
+void wr(int x)
+{
+	if (x < 0)
+	{
+		putchar('-'), x = -x;
+	}
+	if (x > 9)
+	{
+		wr(x / 10);
+	}
+	putchar(x % 10 + '0');
 }
-int a[maxn], xor_sum[maxn], c[maxn][BIT + 2];
-int lp[maxn], rp[maxn];
-int sta[maxn], top;
+
+int a[114514], xor_sum[114514], c[114514][30 + 2];
+int lp[114514], rp[114514];
+int sta[114514], top;
 int T, n;
 int ans;
 int i, j;
-int main() {
-    T = rd();
 
-    while (T--) {
-        n = rd();
-        ans = 0;
-        memset(c, 0, sizeof(c));
-
-        for (i = 2; i <= n + 1; ++i) {
-            scanf("%d", &a[i]), xor_sum[i] = xor_sum[i - 1] ^ a[i];
-
-            for (j = 0; j < BIT; j++)
-                c[i][j] = c[i - 1][j] + (bool)(xor_sum[i] & (1 << j));
-        }
-
-        top = 0, sta[top] = 1;
-
-        for (i = 2; i <= n + 1; ++i) {
-            while (top && a[sta[top]] < a[i])
-                --top;
-
-            lp[i] = sta[top], sta[++top] = i;
-        }
-
-        top = 0, sta[0] = n + 2;
-
-        for (i = n + 1; i >= 2; i--) {
-            while (top && a[sta[top]] <= a[i])
-                --top;
-
-            rp[i] = sta[top], sta[++top] = i;
-        }
-
-        for (i = 2; i <= n + 1; i++)
-            for (j = 0; j < BIT; j++)
-                ans = (ans + ((ll)(c[i - 1][j] - c[lp[i] - 1][j]) * (rp[i] - i - c[rp[i] - 1][j] + c[i - 1][j])
-                              + (ll)(i - lp[i] - c[i - 1][j] + c[lp[i] - 1][j]) * (c[rp[i] - 1][j] - c[i - 1][j])) % mod
-                       * (1 << j) % mod * a[i]) % mod;
-
-        wr(ans), putchar('\n');
-    }
+int main()
+{
+	T = rd();
+	while (T--)
+	{
+		n = rd();
+		ans = 0;
+		memset(c, 0, sizeof(c));
+		for (i = 2; i <= n + 1; ++i)
+		{
+			scanf("%d", &a[i]), xor_sum[i] = xor_sum[i - 1] ^ a[i];
+			for (j = 0; j < 30; j++)
+			{
+				c[i][j] = c[i - 1][j] + ((xor_sum[i] & (1 << j))!=0);
+			}
+		}
+		top = 0, sta[top] = 1;
+		for (i = 2; i <= n + 1; ++i)
+		{
+			while (top && a[sta[top]] < a[i])
+			{
+				--top;
+			}
+			lp[i] = sta[top], sta[++top] = i;
+		}
+		top = 0, sta[0] = n + 2;
+		for (i = n + 1; i >= 2; i--)
+		{
+			while (top && a[sta[top]] <= a[i])
+			{
+				--top;
+			}
+			rp[i] = sta[top], sta[++top] = i;
+		}
+		for (i = 2; i <= n + 1; i++)
+		{
+			for (j = 0; j < 30; j++)
+			{
+				ans = (ans + ((long long)(c[i - 1][j] - c[lp[i] - 1][j]) * (rp[i] - i - c[rp[i] - 1][j] + c[i - 1][j])
+				              + (long long)(i - lp[i] - c[i - 1][j] + c[lp[i] - 1][j]) * (c[rp[i] - 1][j] - c[i - 1][j])) % 1000000061ll
+				       * (1 << j) % 1000000061ll * a[i]) % 1000000061ll;
+			}
+		}
+		wr(ans), putchar('\n');
+	}
 }
