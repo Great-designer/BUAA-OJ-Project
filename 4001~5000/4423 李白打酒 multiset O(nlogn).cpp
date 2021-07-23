ce303697cb950121e8f3a3e1ea944bb559172eef
Great-designer <63183399+Great-designer@users.noreply.github.com>
2021-07-23 13:15:26 +0800

    整理格式

--- 4001~5000/4423 李白打酒 multiset O(nlogn).cpp
+++ 4001~5000/4423 李白打酒 multiset O(nlogn).cpp
@@ -1,67 +1,80 @@
 #include<stdio.h>
 #include<string.h>
+
 #include<set>
 #include<algorithm>
-#define getchar getchar_unlocked
-#define putchar putchar_unlocked
-using namespace std;
-typedef long long ll;
-typedef multiset<ll>::iterator IT;
-ll rd() {
-    int k = 0;
-    char c = getchar();
-    while (c < '0' || c > '9')
-        c = getchar();
 
-    while (c >= '0' && c <= '9') {
-        k = (k << 1) + (k << 3) + (c ^ 48);
-        c = getchar();
-    }
-    return k;
+using namespace std;
+
+long long rd()
+{
+	int k = 0;
+	char c = getchar();
+	while (c < '0' || c > '9')
+		c = getchar();
+	while (c >= '0' && c <= '9')
+	{
+		k = (k << 1) + (k << 3) + (c ^ 48);
+		c = getchar();
+	}
+	return k;
 }
-void wr(ll x) {
-    if (x < 0) putchar('-'), x = -x;
-    if (x > 9)wr(x / 10);
-    putchar(x % 10 + '0');
+
+void wr(long long x)
+{
+	if (x < 0) putchar('-'), x = -x;
+	if (x > 9)wr(x / 10);
+	putchar(x % 10 + '0');
 }
-void print_str(const char* s) {
-    int len = strlen(s), i = 0;
-    while(i < len) putchar(s[i++]);
+
+void print_str(const char* s)
+{
+	int len = strlen(s), i = 0;
+	while(i < len) putchar(s[i++]);
 }
-multiset<ll> wine;
+
+multiset<long long> wine;
 int n;
-ll sum, v;
+long long sum, v;
 int op, x;
 
-int main() {
-    sum = v = rd();
-    n = rd();
-    wine.insert(v);
-    for(int i = 1; i <= n; ++i) {
-        op = rd();
-        if(op == 1) {
-            IT it = wine.upper_bound(0);
-            if(it != wine.end()) {
-                ll target_wine = *it, after = min(target_wine << 1, v);
-                sum += (after - target_wine);
-                wine.erase(it), wine.insert(after);
-            }
-            print_str("Day "), wr(i), print_str(": "), wr(sum), putchar('\n');
-        }
-        else if(op == 2) {
-            IT it = wine.end(); --it;
-            if(*it == 0) print_str("Day "), wr(i), print_str(": "), puts("Such beautiful flowers, what a pity!");
-            else {
-                sum--;
-                ll after = (*it) - 1;
-                wine.erase(it), wine.insert(after);
-                print_str("Day "), wr(i), print_str(": "), wr(sum), putchar('\n');
-            }
-        }
-        else {
-            wine.insert(v);
-            sum += v;
-            print_str("Day "), wr(i), print_str(": "), wr(sum), putchar('\n');
-        }
-    }
+int main()
+{
+	sum = v = rd();
+	n = rd();
+	wine.insert(v);
+	for(int i = 1; i <= n; ++i)
+	{
+		op = rd();
+		if(op == 1)
+		{
+			multiset<long long>::iterator it = wine.upper_bound(0);
+			if(it != wine.end())
+			{
+				long long target_wine = *it, after = min(target_wine << 1, v);
+				sum += (after - target_wine);
+				wine.erase(it), wine.insert(after);
+			}
+			print_str("Day "), wr(i), print_str(": "), wr(sum), putchar('\n');
+		}
+		else if(op == 2)
+		{
+			multiset<long long>::iterator it = wine.end();
+			--it;
+			if(*it == 0) print_str("Day "), wr(i), print_str(": "), puts("Such beautiful flowers, what a pity!");
+			else
+			{
+				sum--;
+				long long after = (*it) - 1;
+				wine.erase(it), wine.insert(after);
+				print_str("Day "), wr(i), print_str(": "), wr(sum), putchar('\n');
+			}
+		}
+		else
+		{
+			wine.insert(v);
+			sum += v;
+			print_str("Day "), wr(i), print_str(": "), wr(sum), putchar('\n');
+		}
+	}
 }
