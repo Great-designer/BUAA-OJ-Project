76b413c44ef554cb0559c9fb0c414a609f358f97
GoatGirl98 <47886494+GoatGirl98@users.noreply.github.com>
2021-08-08 21:26:04 +0800

    Update 825 ModricWang的星际旅行（中难）.c

--- 1~1000/825 ModricWang的星际旅行（中难）.c
+++ 1~1000/825 ModricWang的星际旅行（中难）.c
@@ -1,71 +1,40 @@
-#include<stdio.h>
+#include<stdio.h> 
 #include<string.h>
+#define min(a, b) (a < b ? a : b)
+#define N 16
+//假定无穷距离
+const int INF = 0x3f3f3f3f;
+//maxs = 2 ^ 20 = 1048576
+int dp[(1 << N) + 3][N + 3];
+int a[N + 3][N + 3];
+int i, j, k;
+int n, maxs;//maxs : 最多的状态数
+int main() {
+	while (scanf("%d", &n) != EOF) {
+		for (i = 0; i < n; i++)
+			for (j = 0; j < n; j++)
+				scanf("%d", &a[i][j]);
+		/*
+		//floyd : 求所有点两两之间的最短路
+		for (k = 0; k < n; ++k)
+			for (i = 0; i < n; i++)
+				for (j = 0; j < n; j++)
+					a[i][j] = min(a[i][j], a[i][k] + a[k][j]);
+		*/
+		memset(dp, 0x3f, sizeof(dp));
 
-int dp[65540][20];
-int a[20][20];
-
-int main()
-{
-	int n;
-	while(~scanf("%d",&n))
-	{
-		n--;
-		int i;
-		for(i=0;i<=n;i++)
-		{
-			int j;
-			for(j=0;j<=n;j++)
-			{
-				scanf("%d",&a[i][j]);
-			}
-		}
-		int k;
-		for(k=0;k<=n;k++)
-		{
-			for(i=0;i<=n;i++)
-			{
-				int j;
-				for(j=0;j<=n;j++)
-				{
-					a[i][j]=a[i][j]<(a[i][k]+a[k][j])?a[i][j]:(a[i][k]+a[k][j]);
-				}
-			}
-		}
-		int m=(1<<n)-1;
-		int S;
-		for(S=0;S<=m;S++)
-		{
-			int i;
-			for(i=1;i<=n;i++)
-			{
-				if(S&(1<<(i-1)))
-				{
-					if(S==(1<<(i-1)))
-					{
-						dp[S][i]=a[0][i];
-					}
-					else
-					{
-						dp[S][i]=(1<<30);
-						int j;
-						for(j=1;j<=n;j++)
-						{
-							if(S&(1<<(j-1))&&j!=i)
-							{
-								dp[S][i]=dp[S][i]<(dp[S^(1<<(i-1))][j]+a[j][i])?dp[S][i]:(dp[S^(1<<(i-1))][j]+a[j][i]);
-							}
-						}
-					}
-				}
-			}
-		}
-		int ans=(1<<31)-1;
-		int j;
-		for(j=1;j<=n;j++)
-		{
-			ans=ans<(dp[(1<<n)-1][j]+a[j][0])?ans:(dp[(1<<n)-1][j]+a[j][0]);
-		}
-		printf("%d\n",ans);
+		dp[1][0] = 0;
+		maxs = (1 << n) - 1;
+		for (i = 1; i <= maxs; ++i)
+			for (j = 0; j < n; ++j)
+				if ((i >> j) & 1)
+					for (k = 0; k < n; ++k)
+						if (((i ^ (1 << j)) >> k) & 1)
+							dp[i][j] = min(dp[i][j], dp[i ^ (1 << j)][k] + a[k][j]);
+		int ans = INF;
+		for (i = 1; i < n; ++i)
+			ans = min(ans, dp[(1 << n) - 1][i] + a[i][0]);
+		printf("%d\n", ans);
+		//printf("%d\n", dp[(1 << n) - 1][n - 1]);
 	}
-	return 0;
 }
