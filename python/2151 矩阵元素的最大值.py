aab82cb495e87278c93ea468c02ac286c1e7c98f
zhanch <1214007828@qq.com>
2021-08-17 00:06:55 +0800

    Update 2151 矩阵元素的最大值.py

--- python/2151 矩阵元素的最大值.py
+++ python/2151 矩阵元素的最大值.py
@@ -5,7 +5,7 @@ for i in range(1,n+1):
     lis[1][i]=2-(i%2)
     lis[i][1]=2-(i%2)
 for i in range(2,n+1):
-    for j in range(i,n+1):     //对称阵
+    for j in range(i,n+1):     # 对称阵
         lis[i][j]=lis[i-1][j]+lis[i][j-1]
         lis[j][i]=lis[i][j]
 print(lis[n][n])
