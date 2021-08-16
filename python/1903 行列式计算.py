72604de2db76901f7a1bd42cc7a4c6877d8d6ab1
zhanch <1214007828@qq.com>
2021-08-16 23:34:37 +0800

    Create 1903 行列式计算.py

---
+++ python/1903 行列式计算.py
@@ -0,0 +1,5 @@
+a11, a12, a13 = map(int,input().split())
+a21, a22, a23 = map(int,input().split())
+a31, a32, a33 = map(int,input().split())
+print(a11*a22*a33+a12*a23*a31+a13*a21*a32\
+    -a13*a22*a31-a12*a21*a33-a11*a23*a32)
