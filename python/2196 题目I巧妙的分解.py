299145b6adcc27fdc2d40c0ab9e44a31e80a4b92
Halfoon <1214007828@qq.com>
2021-08-18 11:59:20 +0800

    Create 2196 题目I巧妙的分解.py

---
+++ python/2196 题目I巧妙的分解.py
@@ -0,0 +1,13 @@
+n = int(input())
+if n >= 3:
+    print("YES")
+    lis = [i for i in range(1, n + 1)]
+    if n % 2 == 0:
+        print("1", 2)
+        print(n - 1, 1,*lis[2:])
+    else:
+        lis.remove((n + 1) // 2)
+        print("1", (n + 1) // 2)
+        print(n - 1, *lis)
+else:
+    print("impossible!")
