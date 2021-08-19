436120e88b32595c147119bdcd134b5ec940fdc4
GoatGirl98 <47886494+GoatGirl98@users.noreply.github.com>
2021-08-19 21:08:36 +0800

    Update 753 来一场华丽的学级裁判吧.c

--- 1~1000/753 来一场华丽的学级裁判吧.c
+++ 1~1000/753 来一场华丽的学级裁判吧.c
@@ -1,72 +1,79 @@
-#include<stdio.h>
-
-long long fac[100010],ifac[100010];
-
-long long C(int n,int m)
+#include <stdio.h>
+#define maxn 100000
+const int mod = 1000000007;
+int fac[maxn + 10], ifac[maxn + 10];
+char eof_flag;
+char rd(int *s)
 {
-    if(m>n)
-	{
-		return 0;
-	}
-    long long ret=(fac[n]*ifac[m])%1000000007;
-    ret=(ret*ifac[n-m])%1000000007;
-    return ret;
-}
-
-long long ApowBmodP(long long a,long long b,long long p)
-{
-    long long ret=1;
-    while(b)
-	{
-        if(b&1)
-		{
-			ret=ret*a%p;
-		}
-        a=a*a%p;
-        b>>=1;
+    if (eof_flag)
+        return 0;
+    int k = 0, f = 1;
+    char c = getchar();
+    while (c != '-' && (c < '0' || c > '9'))
+    {
+        if (c == EOF)
+        {
+            eof_flag = 1;
+            return 0;
+        }
+        c = getchar();
     }
-    return ret%p;
+    f = (c == '-') ? -1 : 1;
+    k = (c == '-') ? 0 : (c ^ 48);
+    c = getchar();
+    while (c >= '0' && c <= '9')
+        k = (k << 1) + (k << 3) + (c ^ 48), c = getchar();
+    if (c == EOF)
+        eof_flag = 1;
+    (*s) = f > 0 ? k : -k;
+    return 1;
 }
-
-void buildfacifacinv()
+void wr(int x)
 {
-    fac[0]=1;
-    int i;
-    for(i=1;i<100010;++i)
-    {
-    	fac[i]=(fac[i-1]*i)%1000000007;
-	}
-    ifac[100010-1]=ApowBmodP(fac[100010-1],1000000005,1000000007);
-    for(i=100010-2;i>=0;--i)
-    {
-    	ifac[i]=(ifac[i+1]*(i+1))%1000000007;
-	}
+    if (x > 9)
+        wr(x / 10);
+    putchar(x % 10 + 48);
 }
-
-int a,b,k;
-long long ans;
-
+int C(int n, int m)
+{
+    if (m > n || m < 0)
+        return 0;
+    else
+        return 1ll * fac[n] * (1ll * ifac[m] * ifac[n - m] % mod) % mod;
+}
+int inv(int a, int p)
+{
+    int _a = a, _b = p, u = 1, v = 0;
+    while (_b)
+    {
+        int t = _a / _b;
+        _a -= t * _b;
+        if (_a ^ _b)
+            _a ^= _b, _b ^= _a, _a ^= _b;
+        u -= t * v;
+        if (u ^ v)
+            u ^= v, v ^= u, u ^= v;
+    }
+    u = 1ll * (u + p) % p;
+    return u;
+}
+int solve(int a, int b, int k)
+{
+    if ((k - b) & 1)
+        return 0;
+    else
+        return (1ll * C(k - 1, (k - b) >> 1) - 1ll * C(k - 1, (k + b) >> 1) - 1ll * C(k - 1, a - 1 + ((k + b) >> 1)) + 3ll * mod) % mod;
+}
+int a, b, k;
 int main()
 {
-    buildfacifacinv();
-    while(~scanf("%d%d%d",&a,&b,&k))
-	{
-        if(k<b)
-		{
-			puts("0");
-		}
-        else if((k-b)&1)
-		{
-			puts("0");
-		}
-        else
-		{
-            ans=((2LL*b)%1000000007*ApowBmodP(k+b,1000000005,1000000007))%1000000007*C(k-1,(k-b)>>1)%1000000007;
-            while(ans<=0)
-			{
-				ans+=1000000007;
-			}
-            printf("%lld\n",ans%1000000007);
-        }
-    }
+    fac[0] = 1;
+    for(int i = 1; i <= maxn; ++i)
+        fac[i] = 1ll * fac[i - 1] * i % mod;
+    ifac[maxn] = inv(fac[maxn], mod);
+    for(int i = maxn - 1; i >= 0; --i)
+        ifac[i] = 1ll * ifac[i + 1] * (i + 1) % mod;
+
+    while(rd(&a) && rd(&b) && rd(&k))
+        wr(solve(a, b, k)), putchar('\n');
 }
