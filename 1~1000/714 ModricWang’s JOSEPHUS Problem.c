32a9aa4b94ab9bd4c6e099de95fc69353fd39e56
GoatGirl98 <47886494+GoatGirl98@users.noreply.github.com>
2021-10-24 13:00:14 +0800

    Update 714 ModricWang’s JOSEPHUS Problem.c

--- 1~1000/714 ModricWang’s JOSEPHUS Problem.c
+++ 1~1000/714 ModricWang’s JOSEPHUS Problem.c
@@ -1,22 +1,18 @@
-#include<stdio.h>
-
-int n,m;
-
-int cir(int n,int m)
-{
-	int p=0;
-	int i;
-	for(i=2;i<=n;i++)
-	{
-		p=(p+m)%i;
-	}
-	return p+1;
-}
-
+#include <stdio.h>
 int main()
 {
-	while(~scanf("%d%d%d",&n,&m))
-	{
-		printf("%d\n",cir(n,m));
-	}
+    long long n, k, i, x = 0, y;
+    scanf("%lld%lld", &n, &k);
+    for (i = 2; i <= k && i <= n; ++i)
+        x = (x + k) % i;
+    for (; i <= n; ++i)
+    {
+        y = (i - x - 1) / k;
+        if (i + y > n)
+            y = n - i;
+        i += y;
+        x = (x + (y + 1) % i * k) % i;
+    }
+    printf("%lld\n", x + 1);
+    return 0;
 }
