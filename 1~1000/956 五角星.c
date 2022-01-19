a06e9e6da1657c83c238272326fbd134b217463d
GoatGirl98 <47886494+GoatGirl98@users.noreply.github.com>
2022-01-19 18:51:07 +0800

    Add files via upload

---
+++ 1~1000/956 五角星.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int n, t;
+    scanf("%d", &t);
+    while (t--)
+    {
+        scanf("%d", &n);
+        printf("%d\n", n % 72 ? 32 : 7);
+    }
+    return 0;
+}
