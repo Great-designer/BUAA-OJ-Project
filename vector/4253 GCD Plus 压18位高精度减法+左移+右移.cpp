836d1b2ef415dc6cfe937950dd302c690b98782b
Great-designer <63183399+Great-designer@users.noreply.github.com>
2022-01-25 19:45:50 +0800

    整理代码

--- vector/4253 GCD Plus 压18位高精度减法+左移+右移.cpp
+++ vector/4253 GCD Plus 压18位高精度减法+左移+右移.cpp
@@ -2,105 +2,120 @@
 #include <stdio.h>
 #include <vector>
 using namespace std;
-typedef unsigned long long ull;
-struct bignum {
-    static const ull base = 1000000000000000000ull;
-    vector<ull> a;
-    inline void in() {
-        static char s[10001];
-        scanf("%s", s);
 
-        for (int n = strlen(s), i = 0;;) {
-            a.push_back(0);
+struct bignum
+{
+	static const unsigned long long base = 1000000000000000000ull;
+	vector<unsigned long long> a;
+	inline void in()
+	{
+		static char s[10001];
+		scanf("%s", s);
 
-            if (n - i <= 18) {
-                for (int j = 0; i + j < n; a.back() = (a.back() << 3) + (a.back() << 1) + (s[j] ^ 48), ++j);
+		for (int n = strlen(s), i = 0;;)
+		{
+			a.push_back(0);
 
-                break;
-            }
+			if (n - i <= 18)
+			{
+				for (int j = 0; i + j < n; a.back() = (a.back() << 3) + (a.back() << 1) + (s[j] ^ 48), ++j);
 
-            i += 18;
+				break;
+			}
 
-            for (int j = 0; j < 18; a.back() = (a.back() << 3) + (a.back() << 1) + (s[n - i + j] ^ 48), ++j);
-        }
-    }
-    inline void out() const {
-        printf("%llu", a.back());
+			i += 18;
 
-        if (a.size() >= 2)
-            for (int i = a.size() - 2; i >= 0; printf("%018llu", a[i--]));
-    }
-    inline int cmp(const bignum &b) const {
-        if (a.size() ^ b.a.size())
-            if (a.size() < b.a.size())
-                return -1;
-            else
-                return 1;
+			for (int j = 0; j < 18; a.back() = (a.back() << 3) + (a.back() << 1) + (s[n - i + j] ^ 48), ++j);
+		}
+	}
+	inline void out() const
+	{
+		printf("%llu", a.back());
 
-        for (int i = a.size() - 1; i >= 0; --i)
-            if (a[i] ^ b.a[i])
-                if (a[i] < b.a[i])
-                    return -1;
-                else
-                    return 1;
+		if (a.size() >= 2)
+			for (int i = a.size() - 2; i >= 0; printf("%018llu", a[i--]));
+	}
+	inline int cmp(const bignum &b) const
+	{
+		if (a.size() ^ b.a.size())
+			if (a.size() < b.a.size())
+				return -1;
+			else
+				return 1;
 
-        return 0;
-    }
-    // divide or multiply 2
-    inline void chu() {
-        for (int i = a.size() - 1; i >= 0; (a[i] & 1) && (a[i - 1] += base), a[i--] >>= 1);
+		for (int i = a.size() - 1; i >= 0; --i)
+			if (a[i] ^ b.a[i])
+				if (a[i] < b.a[i])
+					return -1;
+				else
+					return 1;
 
-        for (; !a.back(); a.pop_back());
-    }
-    inline void cheng() {
-        for (int i = 0; i < a.size(); a[i++] <<= 1);
+		return 0;
+	}
+	//除或乘2
+	inline void chu()
+	{
+		for (int i = a.size() - 1; i >= 0; (a[i] & 1) && (a[i - 1] += base), a[i--] >>= 1);
 
-        for (int i = 0; i < a.size() - 1; a[i + 1] += a[i] / base, a[i] %= base, ++i);
+		for (; !a.back(); a.pop_back());
+	}
+	inline void cheng()
+	{
+		for (int i = 0; i < a.size(); a[i++] <<= 1);
 
-        if (a.back() >= base)
-            a.push_back(a.back() / base), a[a.size() - 2] %= base;
-    }
-    inline void operator-=(const bignum &b) {
-        for (int i = 0; i < b.a.size(); ++i) {
-            a[i] -= b.a[i];
+		for (int i = 0; i < a.size() - 1; a[i + 1] += a[i] / base, a[i] %= base, ++i);
 
-            for (; a[i] >> 63; a[i] += base, --a[i + 1]);
-        }
+		if (a.back() >= base)
+			a.push_back(a.back() / base), a[a.size() - 2] %= base;
+	}
+	inline void operator-=(const bignum &b)
+	{
+		for (int i = 0; i < b.a.size(); ++i)
+		{
+			a[i] -= b.a[i];
 
-        for (; !a.back(); a.pop_back());
-    }
+			for (; a[i] >> 63; a[i] += base, --a[i + 1]);
+		}
+
+		for (; !a.back(); a.pop_back());
+	}
 } a, b;
-bignum gcd(bignum a, bignum b) {
-    int cnt = 0, tmp = 0;
 
-    while (!(a.a[0] & 1) && !(b.a[0] & 1))
-        a.chu(), b.chu(), ++cnt;
+bignum gcd(bignum a, bignum b)
+{
+	int cnt = 0, tmp = 0;
 
-    while (1) {
-        while (!(a.a[0] & 1))
-            a.chu();
+	while (!(a.a[0] & 1) && !(b.a[0] & 1))
+		a.chu(), b.chu(), ++cnt;
 
-        while (!(b.a[0] & 1))
-            b.chu();
+	while (1)
+	{
+		while (!(a.a[0] & 1))
+			a.chu();
 
-        tmp = a.cmp(b);
+		while (!(b.a[0] & 1))
+			b.chu();
 
-        if (!tmp)
-            break;
+		tmp = a.cmp(b);
 
-        if (tmp >> 31)
-            b -= a;
-        else
-            a -= b;
-    }
+		if (!tmp)
+			break;
 
-    while (cnt--)
-        a.cheng();
+		if (tmp >> 31)
+			b -= a;
+		else
+			a -= b;
+	}
 
-    return a;
+	while (cnt--)
+		a.cheng();
+
+	return a;
 }
-int main() {
-    a.in();
-    b.in();
-    gcd(a, b).out();
+
+int main()
+{
+	a.in();
+	b.in();
+	gcd(a, b).out();
 }
