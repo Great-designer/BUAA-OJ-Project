cadab8183d0e8464628559a74d0d3ba812e84baa
Great-designer <63183399+Great-designer@users.noreply.github.com>
2022-01-28 23:38:24 +0800

    Update 4423 李白打酒 Splay O(nlogn).c

--- 4001~5000/4423 李白打酒 Splay O(nlogn).c
+++ 4001~5000/4423 李白打酒 Splay O(nlogn).c
@@ -1,257 +1,270 @@
-// Sol : wzb
-#include <stdio.h>
-#define getchar getchar_unlocked
-#define putchar putchar_unlocked
-#define int long long
-int ch[50100][2], val[50100], cnt[50100], par[50100], siz[50100];
-int root, ncnt;
-void wr(int x)
+#include<stdio.h>
+
+long long ch[50100][2], val[50100], cnt[50100], par[50100], siz[50100];
+long long root, ncnt;
+
+void wr(long long x)
 {
-    if (x < 0)
-        putchar('-'), x = -x;
-    if (x > 9)
-        wr(x / 10);
-    putchar(x % 10 + 48);
-}
-int rd()
-{
-    int k = 0, f = 1;
-    char c = getchar();
-    while (c < '0' || c > '9')
-    {
-        if (c == '-')
-            f = -1;
-        c = getchar();
-    }
-    while (c >= '0' && c <= '9')
-    {
-        k = (k << 1) + (k << 3) + c - 48;
-        c = getchar();
-    }
-    return k * f;
+	if (x < 0)
+		putchar('-'), x = -x;
+	if (x > 9)
+		wr(x / 10);
+	putchar(x % 10 + 48);
 }
 
-int chk(int x)
-{ // 0×ó 1ÓÒ
-    if (ch[par[x]][0] != x)
-        return 1;
-    return 0;
-}
-void pushup(int x)
+long long rd()
 {
-    siz[x] = siz[ch[x][0]] + siz[ch[x][1]] + cnt[x]; //×ó+ÓÒ+×Ô¼ºµÄ¸öÊı
+	long long k = 0, f = 1;
+	char c = getchar();
+	while (c < '0' || c > '9')
+	{
+		if (c == '-')
+			f = -1;
+		c = getchar();
+	}
+	while (c >= '0' && c <= '9')
+	{
+		k = (k << 1) + (k << 3) + c - 48;
+		c = getchar();
+	}
+	return k * f;
 }
-void rotate(int x)
-{
-    int y = par[x], z = par[y], k = chk(x), w = ch[x][k ^ 1];
-    ch[y][k] = w;
-    par[w] = y;
-    ch[z][chk(y)] = x;
-    par[x] = z;
-    ch[x][k ^ 1] = y;
-    par[y] = x;
-    pushup(y);
-    pushup(x);
-}
-void splay(int x, int goal)
-{
-    if (goal == -521)
-        goal = 0;
-    while (par[x] != goal)
-    {
-        int y = par[x], z = par[y];
-        if (z != goal)
-        {
-            if (chk(x) == chk(y))
-                rotate(y);
-            //ÈıµãÒ»Ïß Ğı×ª¸¸½Úµã
-            else
-                rotate(x); //²»È»Ğı×ª×Ô¼º
-        }
-        rotate(x);
-    }
-    if (!goal)
-        root = x; // goalÊÇ0 xµÄ¸¸½ÚµãÊÇ0 Ôò¸ùÎªx
-}
-void insert(int x)
-{
-    int cur = root, p = 0;
-    while (cur && val[cur] != x)
-    {
-        p = cur;
-        cur = ch[cur][x > val[cur]];
-    }
-    if (cur)
-    { //ËµÃ÷ÕÒµ½ÁË
-        cnt[cur]++;
-    }
-    else
-    {
-        cur = ++ncnt;
-        //Èç¹û´æÔÚ¸¸Ç×
-        if (p)
-            ch[p][x > val[p]] = cur;
 
-        ch[cur][0] = ch[cur][1] = 0;
-        val[cur] = x;
-        par[cur] = p;
-        cnt[cur] = siz[cur] = 1;
-    }
-    splay(cur, -521);
-}
-int kth(int k)
+long long chk(long long x)
 {
-    int cur = root;
-    while (1)
-    {
-        if (ch[cur][0] && k <= siz[ch[cur][0]])
-        {
-            cur = ch[cur][0];
-        }
-        else if (k > siz[ch[cur][0]] + cnt[cur])
-        {
-            k -= siz[ch[cur][0]] + cnt[cur];
-            cur = ch[cur][1];
-        }
-        else
-        {
-            return cur;
-        }
-    }
+	// 0å·¦ 1å³
+	if (ch[par[x]][0] != x)
+		return 1;
+	return 0;
 }
-void find(int x)
-{ //Õâ¸öÓĞ¿ÉÄÜÕÒµ½Ç°ÇıÓĞ¿ÉÄÜÕÒµ½ºó¼Ì
-    int cur = root;
-    while (ch[cur][x > val[cur]] && x != val[cur])
-    {
-        cur = ch[cur][x > val[cur]];
-    }
-    splay(cur, -521);
-}
-int pre(int x)
+
+void pushup(long long x)
 {
-    find(x);
-    if (val[root] < x)
-        return root;
-    int cur = ch[root][0];
-    while (ch[cur][1])
-        cur = ch[cur][1];
-    return cur;
+	siz[x] = siz[ch[x][0]] + siz[ch[x][1]] + cnt[x]; //å·¦+å³+è‡ªå·±çš„ä¸ªæ•°
 }
-int succ(int x)
+
+void rotate(long long x)
 {
-    find(x);
-    if (val[root] > x)
-        return root;
-    int cur = ch[root][1];
-    while (ch[cur][0])
-        cur = ch[cur][0];
-    return cur;
+	long long y = par[x], z = par[y], k = chk(x), w = ch[x][k ^ 1];
+	ch[y][k] = w;
+	par[w] = y;
+	ch[z][chk(y)] = x;
+	par[x] = z;
+	ch[x][k ^ 1] = y;
+	par[y] = x;
+	pushup(y);
+	pushup(x);
 }
-void Remove(int x)
+
+void splay(long long x, long long goal)
 {
-    int las = pre(x), nxt = succ(x);
-    splay(las, -521);
-    splay(nxt, las);
-    int del = ch[nxt][0];
-    if (cnt[del] > 1)
-    {
-        cnt[del]--;
-        splay(del, -521);
-    }
-    else
-    {
-        ch[nxt][0] = 0;
-        cnt[del] = 0;
-        siz[del] = 0;
-        val[del] = 0;
-        ch[del][0] = ch[del][1] = 0;
-        par[del] = 0;
-    }
+	if (goal == -521)
+		goal = 0;
+	while (par[x] != goal)
+	{
+		long long y = par[x], z = par[y];
+		if (z != goal)
+		{
+			if (chk(x) == chk(y))
+				rotate(y);
+			//ä¸‰ç‚¹ä¸€çº¿ æ—‹è½¬çˆ¶èŠ‚ç‚¹
+			else
+				rotate(x); //ä¸ç„¶æ—‹è½¬è‡ªå·±
+		}
+		rotate(x);
+	}
+	if (!goal)
+		root = x; // goalæ˜¯0 xçš„çˆ¶èŠ‚ç‚¹æ˜¯0 åˆ™æ ¹ä¸ºx
 }
-int get_rank(int x)
+
+void insert(long long x)
 {
-    find(x);
-    if (val[root] < x)
-        return cnt[root] + siz[ch[root][0]];
-    return siz[ch[root][0]];
+	long long cur = root, p = 0;
+	while (cur && val[cur] != x)
+	{
+		p = cur;
+		cur = ch[cur][x > val[cur]];
+	}
+	if (cur)
+	{
+		//è¯´æ˜æ‰¾åˆ°äº†
+		cnt[cur]++;
+	}
+	else
+	{
+		cur = ++ncnt;
+		//å¦‚æœå­˜åœ¨çˆ¶äº²
+		if (p)
+			ch[p][x > val[p]] = cur;
+
+		ch[cur][0] = ch[cur][1] = 0;
+		val[cur] = x;
+		par[cur] = p;
+		cnt[cur] = siz[cur] = 1;
+	}
+	splay(cur, -521);
 }
+
+long long kth(long long k)
+{
+	long long cur = root;
+	while (1)
+	{
+		if (ch[cur][0] && k <= siz[ch[cur][0]])
+		{
+			cur = ch[cur][0];
+		}
+		else if (k > siz[ch[cur][0]] + cnt[cur])
+		{
+			k -= siz[ch[cur][0]] + cnt[cur];
+			cur = ch[cur][1];
+		}
+		else
+		{
+			return cur;
+		}
+	}
+}
+
+void find(long long x)
+{
+	//è¿™ä¸ªæœ‰å¯èƒ½æ‰¾åˆ°å‰é©±æœ‰å¯èƒ½æ‰¾åˆ°åç»§
+	long long cur = root;
+	while (ch[cur][x > val[cur]] && x != val[cur])
+	{
+		cur = ch[cur][x > val[cur]];
+	}
+	splay(cur, -521);
+}
+
+long long pre(long long x)
+{
+	find(x);
+	if (val[root] < x)
+		return root;
+	long long cur = ch[root][0];
+	while (ch[cur][1])
+		cur = ch[cur][1];
+	return cur;
+}
+
+long long succ(long long x)
+{
+	find(x);
+	if (val[root] > x)
+		return root;
+	long long cur = ch[root][1];
+	while (ch[cur][0])
+		cur = ch[cur][0];
+	return cur;
+}
+
+void Remove(long long x)
+{
+	long long las = pre(x), nxt = succ(x);
+	splay(las, -521);
+	splay(nxt, las);
+	long long del = ch[nxt][0];
+	if (cnt[del] > 1)
+	{
+		cnt[del]--;
+		splay(del, -521);
+	}
+	else
+	{
+		ch[nxt][0] = 0;
+		cnt[del] = 0;
+		siz[del] = 0;
+		val[del] = 0;
+		ch[del][0] = ch[del][1] = 0;
+		par[del] = 0;
+	}
+}
+
+long long get_rank(long long x)
+{
+	find(x);
+	if (val[root] < x)
+		return cnt[root] + siz[ch[root][0]];
+	return siz[ch[root][0]];
+}
+
 signed main()
 {
-    int t, op, num = 3, v;
-    v = rd();
-    t = rd();
-    insert(2020202002002020ll);
-    insert(-2020202002002020ll);
-    insert(v);
-    int sum = v;
-    int i;
-    for (i = 1; i <= t; i++)
-    {
-        op = rd();
-        putchar('D');
-        putchar('a');
-        putchar('y');
-        putchar(' ');
-        wr(i);
-        putchar(':');
-        putchar(' ');
-        switch (op)
-        {
-        case 1:
-        {
-            if (sum == 0)
-            {
-                puts("0");
-                continue;
-            }
-            int tmp = succ(0);
-            if (val[tmp] * 2 <= v)
-            {
-                int tt = val[tmp];
-                Remove(val[tmp]);
-                tt *= 2;
-                insert(tt);
-                sum += tt / 2;
-                wr(sum);
-            }
-            else
-            {
-                sum -= val[tmp];
-                Remove(val[tmp]);
-                int tt = tmp;
-                tt = v;
-                insert(tt);
-                sum += tt;
-                wr(sum);
-            }
-            break;
-        }
-        case 2:
-        {
-            if (sum == 0)
-            {
-                puts("Such beautiful flowers, what a pity!");
-                continue;
-            }
-            int kt = kth(num - 1);
-            int tt = val[kt];
-            Remove(val[kt]);
-            tt--;
-            insert(tt);
-            sum -= 1;
-            wr(sum);
-            break;
-        }
-        case 3:
-        {
-            num++;
-            sum += v;
-            insert(v);
-            wr(sum);
-            break;
-        }
-        }
-        putchar(10);
-    }
+	long long t, op, num = 3, v;
+	v = rd();
+	t = rd();
+	insert(2020202002002020ll);
+	insert(-2020202002002020ll);
+	insert(v);
+	long long sum = v;
+	long long i;
+	for (i = 1; i <= t; i++)
+	{
+		op = rd();
+		putchar('D');
+		putchar('a');
+		putchar('y');
+		putchar(' ');
+		wr(i);
+		putchar(':');
+		putchar(' ');
+		switch (op)
+		{
+			case 1:
+			{
+				if (sum == 0)
+				{
+					puts("0");
+					continue;
+				}
+				long long tmp = succ(0);
+				if (val[tmp] * 2 <= v)
+				{
+					long long tt = val[tmp];
+					Remove(val[tmp]);
+					tt *= 2;
+					insert(tt);
+					sum += tt / 2;
+					wr(sum);
+				}
+				else
+				{
+					sum -= val[tmp];
+					Remove(val[tmp]);
+					long long tt = tmp;
+					tt = v;
+					insert(tt);
+					sum += tt;
+					wr(sum);
+				}
+				break;
+			}
+			case 2:
+			{
+				if (sum == 0)
+				{
+					puts("Such beautiful flowers, what a pity!");
+					continue;
+				}
+				long long kt = kth(num - 1);
+				long long tt = val[kt];
+				Remove(val[kt]);
+				tt--;
+				insert(tt);
+				sum -= 1;
+				wr(sum);
+				break;
+			}
+			case 3:
+			{
+				num++;
+				sum += v;
+				insert(v);
+				wr(sum);
+				break;
+			}
+		}
+		putchar(10);
+	}
 }
